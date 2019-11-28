af05e69a0f741af03c118b664cf936403865887d
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-11-27 20:38:23 -0500

    Update personalproject.js

--- personalproject.js
+++ personalproject.js
@@ -1,3 +1,13 @@
-addtask() {
-  
+document.getElementById("text").addEventListener('click', add);
+var addRect = function(){
+   var ok = true;
+
+   if (ok === true) {
+      var div = document.createElement('div');
+
+      div.idName = "add";       
+      document.getElementsByTagName('body')[0].appendChild(div);
+   }
 }
+
+
