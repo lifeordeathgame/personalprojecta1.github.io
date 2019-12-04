fb1096e18c34de4cee0800834e348b2a0b781af3
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-12-04 08:50:12 -0500

    Update personalproject.js

--- personalproject.js
+++ personalproject.js
@@ -1,3 +1,6 @@
 var addDiv = function(){
    $( "#content" ).append( "<div class='utasks' id='tsk1'></div>" );
 }
+$('.utasks').click(function(){
+  $( "#content" ).append( "<p class='utasktitle'>Yo</p>" );
+});
