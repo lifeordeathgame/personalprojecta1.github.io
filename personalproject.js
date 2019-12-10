f32f655e3829098ebac896a583924c2fb5e23de1
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-12-10 01:56:30 -0500

    Update personalproject.js

--- personalproject.js
+++ personalproject.js
@@ -1,21 +1,45 @@
 var addDiv = function(){
    y = ($('#content .utasks').length+1).toString();
    if ($('#content .utasks').length <= 3){
-      $( "#content" ).append( "<div class='utasks' id='task"+y+"' onclick='addtsk"+y+"()'></div>" );
+      $( "#content" ).append( "<div class='utasks' id='tsk"+y+"' onclick='addtsk"+y+"()'></div>" );
    }
    else {
       window.alert("You already have 4 events! Try deleting one by clicking on it.");
    }
 }
 var addtsk1 = function(){
-   window.alert("fuckyou1");
+  var prmt = prompt("Please enter subject name:", "Math");
+  if (prmt == null || prmt == "") {
+     window.alert("You didn't enter a name")
+     return;
+  } else {
+     $( "#tsk1" ).append( "<p class='utasktitle'>"+prmt+"</p>" );
+  }
 }
 var addtsk2 = function(){
-   window.alert("fuckyou2");
+  var prmt = prompt("Please enter subject name:", "Math");
+  if (prmt == null || prmt == "") {
+     window.alert("You didn't enter a name")
+     return;
+  } else {
+     $( "#tsk2" ).append( "<p class='utasktitle'>"+prmt+"</p>" );
+  }
 }
 var addtsk3 = function(){
-   window.alert("fuckyou3");
+  var prmt = prompt("Please enter subject name:", "Math");
+  if (prmt == null || prmt == "") {
+     window.alert("You didn't enter a name")
+     return;
+  } else {
+     $( "#tsk3" ).append( "<p class='utasktitle'>"+prmt+"</p>" );
+  }
 }
 var addtsk4 = function(){
-   window.alert("fuckyou4");
+  var prmt = prompt("Please enter subject name:", "Math");
+  if (prmt == null || prmt == "") {
+     window.alert("You didn't enter a name")
+     return;
+  } else {
+     $( "#tsk4" ).append( "<p class='utasktitle'>"+prmt+"</p>" );
+  }
 }
