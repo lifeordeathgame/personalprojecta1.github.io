d1e28d077655a08d95fa5bee363e44b4e8090d01
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-12-10 01:44:43 -0500

    Update personalproject.js

--- personalproject.js
+++ personalproject.js
@@ -1,12 +1,22 @@
 var addDiv = function(){
    x = "tsk"+($('#content .utasks').length+1).toString();
+   y = "addtsk"+($('#content .utasks').length+1).toString();
    if ($('#content .utasks').length <= 3){
-      $( "#content" ).append( "<div class='utasks' id='"+x+"'></div>" );
+      $( "#content" ).append( "<div class='utasks' id='"+x+"' onclick='"+y+"'></div>" );
    }
    else {
       window.alert("You already have 4 events! Try deleting one by clicking on it.");
    }
 }
-$('.utasks').click(function() {
-  window.alert('hohoho');
-});
+var addtsk1 = function(){
+   window.alert("fuckyou1");
+}
+var addtsk2 = function(){
+   window.alert("fuckyou2");
+}
+var addtsk3 = function(){
+   window.alert("fuckyou3");
+}
+var addtsk4 = function(){
+   window.alert("fuckyou4");
+}
